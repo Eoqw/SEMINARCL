b34ca6861cb4a43f52f753d40a39f41327787659
Denis Lodygin <Alirka94@mail.ru>
2023-11-27 01:40:22 +0500

    Решили задачу1

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,1 +1,27 @@
-﻿int number1 = 19;
+﻿// Задание 1. 
+// Напишите программу, которая на вход принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+
+
+// string text = "Text";
+// double nubmer = 3.45;
+
+
+System.Console.Write("input number1: ");
+string num1String = Console.ReadLine();
+int number1 = Convert.ToInt32(num1String);
+
+System.Console.Write("input number2: ");
+int nubmer2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (number1 == Math.Pow(nubmer2,2))
+{
+    Console.WriteLine("yes");
+
+}
+else {
+    Console.WriteLine("no");
+}
+
