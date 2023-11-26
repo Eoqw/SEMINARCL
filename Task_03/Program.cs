ae9356c4678de0e8597952dedda140aaf95f2fed
Denis Lodygin <Alirka94@mail.ru>
2023-11-27 03:06:11 +0500

    Добавили задачу 3

---
+++ Task_03/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая прнимает на вход трехзнакчное целое число 
+// и на выходе показывает сумму первой и последней цифры этого числа.
+// 456 =>10
+
+System.Console.WriteLine("Input: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDig = number / 100;
+int secondDig = number % 10;
+
+System.Console.WriteLine($"First dig = {firstDig}. Second dig = {secondDig}. Result = {firstDig+secondDig}");
