880bea90922e9bfb528eaf69aebd4549f19a574b
Denis Lodygin <Alirka94@mail.ru>
2023-11-27 02:53:13 +0500

    добавили задачи на массивы

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая на вход принимает целое число N 
+// А на выходе показывает все целые числа в промежутке от -N до N.
+// Примеры 
+// 4 => -4 -3 -2 -1 0 1 2 3 4 
+
+System.Console.WriteLine("Input: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Number => {number}");
+
+
+// int index = number * (-1);
+
+// while (index <= number)
+// {
+// System.Console.Write (index + " ");
+// index ++;
+// }
+
+int numberAbs = Math.Abs(number); //Модуль MAth возвращает модуль числа если отрицательное то будет положительное
+for (int i = -numberAbs; i <= numberAbs; i++)
+{
+    System.Console.Write(i + " ");
+    
+}
+
+
+
